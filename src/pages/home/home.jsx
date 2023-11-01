5bfc49b6a689248dabda212b7cac017ca8f52af6
AnthonyLomasso <105284519+AnthonyJLM@users.noreply.github.com>
2023-11-01 11:48:09 -0300

    Add files via upload

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import './home.css'
+
+export function Home(){
+    return(
+        <div className = "barra">
+            <header>
+                <h1 className = "titulobar">AGENDA DE MATÉRIAS</h1>
+            </header>
+        </div>
+
+    )
+}
